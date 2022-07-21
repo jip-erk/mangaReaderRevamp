b31f75f4e91a1ec5a42fcbf76c044901ee4efd85
jipErkelens <59746858+jip-erk@users.noreply.github.com>
2022-07-21 19:40:46 +0200

    yes

--- public/js/reusable/list-type.js
+++ public/js/reusable/list-type.js
@@ -1,80 +1,63 @@
-let listTypes = localStorage.getItem("list-types") ?
-    JSON.parse(localStorage.getItem("list-types")) : {};
+let listTypes = localStorage.getItem("list-types")
+	? JSON.parse(localStorage.getItem("list-types"))
+	: {};
 
 function updateListTypes() {
-    // Set default value for each one
-    document.querySelectorAll("[data-list-id]").forEach((section) => {
-        // Default value
-        if (!listTypes[section.dataset.listId])
-            listTypes[section.dataset.listId] = "list"; // "list" or "grid"
+	// Set default value for each one
+	document.querySelectorAll("[data-list-id]").forEach((section) => {
+		// Default value
+		if (!listTypes[section.dataset.listId])
+			listTypes[section.dataset.listId] = "list"; // "list" or "grid"
 
-        // Update attribute
-        section.setAttribute("data-list-type", listTypes[section.dataset.listId]);
-    });
+		// Update attribute
+		section.setAttribute("data-list-type", listTypes[section.dataset.listId]);
+	});
 
-    localStorage.setItem("list-types", JSON.stringify(listTypes));
+	localStorage.setItem("list-types", JSON.stringify(listTypes));
 }
 
 function initListTypes() {
-    document.querySelectorAll("[data-list-id]").forEach((section) => {
-        let listId = section.dataset.listId;
+	document.querySelectorAll("[data-list-id]").forEach((section) => {
+		let listId = section.dataset.listId;
 
+		section.querySelector(".toggle-edit").addEventListener("click", (evt) => {
+			document.querySelector(".toggle-edit").classList.toggle("is-selected");
+			document.querySelector(".delete-butt").classList.toggle("visi");
 
+			document.querySelectorAll(".remove-div").forEach((manga) => {
+				//manga.classList.toggle("remover");
+				manga.classList.toggle("remover");
+			});
+		});
+	});
 
+	// Toggle "show on home"
+	document.querySelectorAll(".toggle-home").forEach((homeButton) => {
+		homeButton.addEventListener("click", (evt) => {
+			evt.preventDefault();
 
+			let listId = homeButton.closest("[id]").id;
+			homeButton.classList.toggle("is-selected");
+			let showOnHome = homeButton.classList.contains("is-selected");
 
-        section.querySelector(".toggle-edit").addEventListener("click", (evt) => {
-            document.querySelector(".toggle-edit").classList.toggle("is-selected");
-            document.querySelector(".delete-butt").classList.toggle("visi");
+			let url = location.href;
+			if (!url.endsWith("/")) url += "/";
 
-            document.querySelectorAll(".manga-meta").forEach((manga) => {
+			fetch(`${url}set-home`, {
+				method: "POST",
+				headers: {
+					"content-type": "application/json",
+				},
+				body: JSON.stringify({
+					listId,
+					value: showOnHome,
+				}),
+			});
+		});
+	});
 
-                manga.classList.toggle("remover");
-
-            });
-
-
-
-            document.querySelectorAll(".hide-from-reading").forEach((manga) => {
-                manga.classList.toggle("visi");
-
-            });
-
-
-        });
-    });
-
-
-
-
-    // Toggle "show on home"
-    document.querySelectorAll(".toggle-home").forEach((homeButton) => {
-        homeButton.addEventListener("click", (evt) => {
-            evt.preventDefault();
-
-            let listId = homeButton.closest("[id]").id;
-            homeButton.classList.toggle("is-selected");
-            let showOnHome = homeButton.classList.contains("is-selected");
-
-            let url = location.href;
-            if (!url.endsWith("/")) url += "/";
-
-            fetch(`${url}set-home`, {
-                method: "POST",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: JSON.stringify({
-                    listId,
-                    value: showOnHome,
-                }),
-            });
-        });
-    });
-
-    updateListTypes();
+	updateListTypes();
 }
 initListTypes();
-
 
 //home info
